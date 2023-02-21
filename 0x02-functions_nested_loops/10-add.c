0dadf0cbf4b4a3187caf527b5aeef3dd3beed8ae
Black-Sparkles <dolapooluwatoyosi.h@gmail.com>
2023-02-21 14:45:17 -0800

    my eleventh commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 
 /**
  * add - Adds
- * @n1 - first number
- * @n2 - second number
+ * @n1 - The first number to be computed
+ * @n2 - The second number to be computed
  * Return: Integer value
  */
 

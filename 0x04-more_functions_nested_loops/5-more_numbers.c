ea94ea01b20a1a35e8d4d56e9b469d1f19b7ec71
Black-Sparkles <dolapooluwatoyosi.h@gmail.com>
2023-02-23 14:09:36 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 lines the numbers from 0 to 10
+ * Return: 10 thimes of numbers 0 to 10
+ */
+
+void more_numbers(void)
+
+{
+	int x, y:
+	
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 10; y++)
+	{
+	if (y > 0)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}

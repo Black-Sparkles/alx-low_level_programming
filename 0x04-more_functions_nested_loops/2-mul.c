48700175d254a923bf472591ccc8cb214d9bbe2c
Black-Sparkles <dolapooluwatoyosi.h@gmail.com>
2023-02-23 13:38:07 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * mul - Multiplies two integers
+ * @x: The first num to multiple
+ * @y: The second num 
+ * Return: The result
+ */
+
+int mul(int x, int y)
+
+{
+
+	return (x * y);
+
+}

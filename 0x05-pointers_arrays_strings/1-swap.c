5f1d33f00ff4c485ff69b81c34b961e1f2794a74
Black-Sparkles <dolapooluwatoyosi.h@gmail.com>
2023-02-27 17:27:00 -0800

    a commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of 2 integers
+ * @a: first integer to be swapped
+ * @b: swconf integer to be swapped
+ *
+ * Return: nothing
+ */
+
+void swap_int(int*a, int*b)
+/* the function that swaps the two integers*/
+{
+	int temp = *a;
+        *a = *b;
+        *b = temp;
+}

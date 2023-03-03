96187604454dff8774c7ad6a66f8b5b20bbd6828
Black-Sparkles <dolapooluwatoyosi.h@gmail.com>
2023-03-02 16:54:42 -0800

    my commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,19 @@
  * _strcmp - compares pointers tO  strings
  * @s1: point to the first string 
  * @s2: point to the second string
- * Return: If str1 < str2, -ive dif
- * If str1 == str2, 0
- * If str1 > str2, +ve diff
+ * Return: If s1 < s2, -ive dif
+ * If s1 == s2, 0
+ * If s1 > s2, +ve diff
  *
  */
 
 int _strcmp(char *s1, char *s2)
 
 {
-
 	while (*s1 && *s2 && *s1 == *s2)
 	{
 	s1++;
 	s2++;
 	}
 	return (*s1 - *s2);
-
 }

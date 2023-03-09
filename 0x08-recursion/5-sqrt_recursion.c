b98f83113c40a48688637847b7a55c6eca1d56cd
Black-Sparkles <dolapooluwatoyosi.h@gmail.com>
2023-03-08 16:44:17 -0800

    my commit today

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,7 +6,6 @@ int _sqrt(int n, int i);
 /**
  * _sqrt_recursion - returns natural square root of x
  * @n: number
- * @i: iterated number
  * Return: the natural square root
  */
 
@@ -14,6 +13,13 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
+
+/**
+ * _sqrt - calcualting square root
+ * @n: number
+ * @i: iterated number
+ * Return: square root
+ */
 
 int _sqrt(int n, int i)
 {

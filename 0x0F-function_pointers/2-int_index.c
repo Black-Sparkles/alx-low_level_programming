da9644ba24ad81d7dda287033fae9d14e9cf7eea
Black-Sparkles <dolapooluwatoyosi.h@gmail.com>
2023-03-24 13:26:49 -0700

    searches for an integer using 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - indexes array
+ * @array: array 
+ * @size: size of array
+ * @cmp: function for size
+ * Return: intial value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]))
+			return (index);
+	}
+	return (-1);
+}
